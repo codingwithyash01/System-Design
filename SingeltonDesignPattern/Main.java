a176da9b97ab4200c8b6e40d44c1be654c23da67
Yash Verma <developeryash01@gmail.com>
2025-05-13 14:07:51 +0530

    Implemented Singleton Design Pattern

---
+++ SingeltonDesignPattern/Main.java
@@ -0,0 +1,12 @@
+package SingeltonDesignPattern;
+
+public class Main {
+    public static void main(String[] args) {
+        Student st1 = Student.getInstance();
+        Student st2 = Student.getInstance();
+        System.out.println(st1);
+        System.out.println(st2);
+        System.out.println(st1==st2);
+    }
+}
+
---
+++ SingeltonDesignPattern/Student.java
@@ -0,0 +1,20 @@
+package SingeltonDesignPattern;
+
+public class Student {
+  
+    private static Student instance;
+    private Student(){
+
+    }
+
+    public static Student getInstance(){
+        if(instance ==null){
+            synchronized(Student.class){
+                if(instance==null){
+                    instance = new Student();
+                }
+            }
+        }
+        return instance;
+    }
+}
